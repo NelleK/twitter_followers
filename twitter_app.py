27d13a38f99d8821ca7351ad106efc1a1aa481a6
Nelle Kooren <nellekooren@gmail.com>
2017-08-23 19:13:22 +0200

    4th x Testing Pycharm push to github and bitbucket separately

--- twitter_app.py
+++ twitter_app.py
@@ -11,6 +11,7 @@ from shutil import move
 # testing git push double from pycharm
 # testing git push double from pycharm 2
 # testing git push double from pycharm 3
+# testing git push double from pycharm 4
 
 def paginate(iterable, page_size):
     while True:
